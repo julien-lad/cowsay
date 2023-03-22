60a70ac540325264e0fc388a9db270d6a3887b4b
julien-lad <julien.ladreyt@gmail.com>
2023-03-22 13:55:58 +0100

    modification pour CommonJS

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import cowsay from "cowsay";
-import userInfo from "./information.js";
-//const userInfo = require('./information');
+const cowsay = require("cowsay");
+const userInfo = require('./information');
 console.log(cowsay.say({
     text : `hello, i'm ${userInfo.longName} and i travel ${userInfo.campus}`,
     e : "oO",
--- information.js
+++ information.js
@@ -1,12 +1,7 @@
 const longName = "Julien Ladreyt";
 const campus = "Valence";
 
-export default{
-    longName: longName, 
-    campus: campus,
-}
-/*
 module.exports = {
     longName: longName, 
     campus: campus,
-}*/
+}
