a8b12cb532a9b921d3313fcb365a7acfc98974e6
ladreyt <julien.ladreyt@gmail.com>
2023-04-23 14:02:43 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cowsay = require("cowsay");
 const userInfo = require('./information');
 console.log(cowsay.say({
-    text : `hello, i'm ${userInfo.longName} and i travel ${userInfo.campus}`,
+    text : `hello, i'm ${userInfo.longName} and i leave ${userInfo.campus}`,
     e : "oO",
     T : "U "
 }));
